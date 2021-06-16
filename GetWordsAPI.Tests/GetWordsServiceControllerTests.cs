557e06880f32d422dedb686c6bda28e6c3110be2
Frank <frankli1009@gmail.com>
2021-06-16 06:46:55 +0100

    Add project GetWordsAPI.Tests

---
+++ GetWordsAPI.Tests/GetWordsServiceControllerTests.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace GetWordsAPI.Tests
+{
+    [Collection("Integration Tests")]
+    public class GetWordsServiceControllerTests
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public GetWordsServiceControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetWords_SuccessOnWholeList()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/gws/skhier/5");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            var responseObject = JsonConvert.DeserializeObject<List<string>>(
+                await response.Content.ReadAsStringAsync());
+            Assert.Equal(720, responseObject?.Count);
+        }
+
+        [Fact]
+        public async Task GetWords_SuccessOnExtraRegExp()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            string extra = "..i..";
+
+            // Act
+            var response = await client.GetAsync("/gws/skhier/5/" +
+                HttpUtility.UrlEncode(extra));
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            var responseObject = JsonConvert.DeserializeObject<List<string>>(
+                await response.Content.ReadAsStringAsync());
+            Assert.Equal(120, responseObject?.Count);
+        }
+
+        [Fact]
+        public async Task GetWords_SuccessOnRejected()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            List<string> rejected = new List<string>();
+            rejected.Add("shire");
+            rejected.Add("shirk");
+            var rejectedJson = JsonConvert.SerializeObject(rejected);
+
+            // Act
+            var response = await client.GetAsync("/gws/skhier/5?rejected=" +
+                HttpUtility.UrlEncode(rejectedJson));
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            var responseObject = JsonConvert.DeserializeObject<List<string>>(
+                await response.Content.ReadAsStringAsync());
+            Assert.Equal(718, responseObject?.Count);
+        }
+
+        [Fact]
+        public async Task GetWords_SuccessOnRegExpAndRejected()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            string extra = "..i..";
+            List<string> rejected = new List<string>();
+            rejected.Add("shire");
+            rejected.Add("shirk");
+            var rejectedJson = JsonConvert.SerializeObject(rejected);
+
+            // Act
+            var response = await client.GetAsync("/gws/skhier/5/" +
+                HttpUtility.UrlEncode(extra) + "?rejected=" +
+                HttpUtility.UrlEncode(rejectedJson));
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            var responseObject = JsonConvert.DeserializeObject<List<string>>(
+                await response.Content.ReadAsStringAsync());
+            Assert.Equal(118, responseObject?.Count);
+        }
+
+        [Theory]
+        [InlineData("/health", "Healthy")]
+        public async Task GetPath_ReturnsSuccessAndExpectedStatus(string path, string expectedStatus)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(path);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.NotNull(response.Content);
+            var responseObject = JsonConvert.DeserializeObject<ResponseType>(
+                await response.Content.ReadAsStringAsync());
+            Assert.Equal(expectedStatus, responseObject?.Status);
+        }
+
+        private class ResponseType
+        {
+            public string Status { get; set; }
+        }
+    }
+}
---
+++ GetWordsAPI.Tests/TestCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace GetWordsAPI.Tests
+{
+    [CollectionDefinition("Integration Tests")]
+    public class TestCollection : ICollectionFixture<WebApplicationFactory<GetWordsAPI.Startup>>
+    {
+        public TestCollection()
+        {
+            // This class has no code, and is never created. Its purpose is simply
+            // to be the place to apply [CollectionDefinition] and all the
+            // ICollectionFixture<> interfaces.
+        }
+    }
+}
